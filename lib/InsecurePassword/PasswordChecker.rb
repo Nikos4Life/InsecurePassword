d776a915c03ff028627c0d5fa82bca67c4f15047
Miguel <mmmx3m@gmail.com>
2015-11-14 16:15:26 +0100

    Fucking master with regex

--- lib/InsecurePassword/PasswordChecker.rb
+++ lib/InsecurePassword/PasswordChecker.rb
@@ -4,10 +4,18 @@ class PasswordChecker
 	end
 	
 	def check_password(email, password)
-		# 
+		if (password =~ /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.D).{8,}/)
+			true
+		else
+			false
+		end
 	end
 	
-	def check_password_length?(password)
-		password.size > 7 ? true : false
-	end
+	# def check_password_length?(password)
+	# 	password.size > 7 ? true : false
+	# end
+
+	# def check_password_charset?(password)
+	# 	password.size > 7 ? true : false
+	# end
 end
--- spec/InsecurePassword_spec.rb
+++ spec/InsecurePassword_spec.rb
@@ -4,16 +4,40 @@ describe InsecurePassword do
   it 'has a version number' do
     expect(InsecurePassword::VERSION).not_to be nil
   end
-describe "#check_password_length?" do
-  it 'checks password length' do
-    pass = PasswordChecker.new
-    expect(pass.check_password_length?("1234567")).to eq(false)
+  describe "#check_password" do
+    it 'checks password length' do
+      pass = PasswordChecker.new
+      expect(pass.check_password("","1234567")).to eq(false)
+    end
   end
-end
-describe "#check_password_length?" do
-  it 'checks password length' do
-    pass = PasswordChecker.new
-    expect(pass.check_password_length?("12345678")).to eq(true)
+    describe "#check_password" do
+    it 'checks password length' do
+      pass = PasswordChecker.new
+      expect(pass.check_password("","aD123@78")).to eq(true)
+    end
   end
-end
+  # describe "#check_password_length?" do
+  #   it 'checks password length' do
+  #     pass = PasswordChecker.new
+  #     expect(pass.check_password_length?("1234567")).to eq(false)
+  #   end
+  # end
+  # describe "#check_password_length?" do
+  #   it 'checks password length' do
+  #     pass = PasswordChecker.new
+  #     expect(pass.check_password_length?("12345678")).to eq(true)
+  #   end
+  # end
+  # describe "#check_password_charset?" do
+  #   it 'checks password charset' do
+  #     pass = PasswordChecker.new
+  #     expect(pass.check_password_charset?("a1@")).to eq(true)
+  #   end
+  # end  
+  # describe "#check_password_charset?" do
+  #   it 'checks password charset' do
+  #     pass = PasswordChecker.new
+  #     expect(pass.check_password_charset?("a1")).to eq(false)
+  #   end
+  # end
 end
