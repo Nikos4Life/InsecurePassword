da215c1a914ff987782642a186bff28cce267950
Miguel <mmmx3m@gmail.com>
2015-11-14 16:55:37 +0100

    Almost done

--- lib/InsecurePassword/PasswordChecker.rb
+++ lib/InsecurePassword/PasswordChecker.rb
@@ -1,16 +1,32 @@
+require 'pry'
 class PasswordChecker
 	def initialize()
 		
 	end
 	
 	def check_password(email, password)
+		if check_password_charset? == true && email_included? == false
+			true
+		else
+			false
+		end
+	end
+	def check_password_charset?(password)
 		if (password =~ /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.D).{8,}/)
 			true
 		else
 			false
 		end
 	end
-	
+	def email_included?(email, password)
+		arr = email.split('@',2)
+		# binding.pry
+		if (password.include?(arr[0]) == false && password.include?(arr[1]) == false)
+			false
+		else
+			true
+		end
+	end
 	# def check_password_length?(password)
 	# 	password.size > 7 ? true : false
 	# end
--- spec/InsecurePassword_spec.rb
+++ spec/InsecurePassword_spec.rb
@@ -1,19 +1,37 @@
 require 'spec_helper'
 require_relative "../lib/InsecurePassword/PasswordChecker.rb"
 describe InsecurePassword do
-  it 'has a version number' do
-    expect(InsecurePassword::VERSION).not_to be nil
-  end
-  describe "#check_password" do
-    it 'checks password length' do
+  # it 'has a version number' do
+  #   expect(InsecurePassword::VERSION).not_to be nil
+  # end
+  # describe "#check_password" do
+  #   it 'checks password length' do
+  #     pass = PasswordChecker.new
+  #     expect(pass.check_password("","1234567")).to eq(false)
+  #   end
+  # end
+  # describe "#check_password" do
+  #   it 'checks password length' do
+  #     pass = PasswordChecker.new
+  #     expect(pass.check_password("","aD123@78")).to eq(true)
+  #   end
+  # end
+  # describe "#email_included?" do
+  #   it 'checks email domain inside password' do
+  #     pass = PasswordChecker.new
+  #     expect(pass.email_included?("aD123@aD123.com","aD123@aD123.com")).to eq(true)
+  #   end
+  # end
+  # describe "#email_included?" do
+  #   it 'checks email domain inside password' do
+  #     pass = PasswordChecker.new
+  #     expect(pass.email_included?("aD123@aD123.com","aD123@78")).to eq(true)
+  #   end
+  # end
+    describe "#email_included?" do
+    it 'checks email domain inside password' do
       pass = PasswordChecker.new
-      expect(pass.check_password("","1234567")).to eq(false)
-    end
-  end
-    describe "#check_password" do
-    it 'checks password length' do
-      pass = PasswordChecker.new
-      expect(pass.check_password("","aD123@78")).to eq(true)
+      expect(pass.check_password("aD123@aD123.com","123aB@78")).to eq(false)
     end
   end
   # describe "#check_password_length?" do
